56d35348ea8eca6d2909330e20c9c96df7a19b02
Ali Rostami <rostamiev@gmail.com>
2024-09-04 13:10:50 +0200

    tools are prepared

--- generate_headers.py
+++ generate_headers.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 def generate_book_table(title, author, year, country, genre, rating):
     table = f"""
 | نام اثر        | {title}       |
@@ -8,3 +12,28 @@ def generate_book_table(title, author, year, country, genre, rating):
 | امتیاز         | {rating}      |
 """
     return table
+
+
+folder_path = "_posts"
+with open('table_of_posts.csv') as csvfile:
+    spamreader = csv.reader(csvfile)
+    next(spamreader)
+    for row in spamreader:
+        file_path = os.path.join(folder_path, row[5] + '-' + row[6])
+        print(file_path)
+        with open(file_path, 'r', encoding='utf-8') as file:
+            file_contents = file.read()
+            if 'نام اثر' not in file_contents:
+                header_for_book = generate_book_table(row[0],row[1],row[2],row[3],'رمان اجتماعی','⭐⭐⭐⭐☆ 4/5')
+
+
+
+# for filename in os.listdir(folder_path):
+#     if filename.endswith(".md"):
+#         file_path = os.path.join(folder_path, filename)
+#         print(file_path)
+#         # Read the file contents
+#         with open(file_path, 'r', encoding='utf-8') as file:
+#             file_contents = file.read()
+#             if 'نام اثر' not in file_contents:
+#                 pass#print(file_path)
