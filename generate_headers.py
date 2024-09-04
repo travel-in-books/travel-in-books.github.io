9f7cdbba3d4d7f3fae1d0521afabef818d07ddf1
Ali Rostami <rostamiev@gmail.com>
2024-09-04 14:05:05 +0200

    change format of headers

--- generate_headers.py
+++ generate_headers.py
@@ -4,12 +4,12 @@ import os
 
 def generate_book_table(title, author, year, country, genre, rating):
     table = f"""
-| نام اثر        | {title}       |
-| نویسنده        | {author}      |
-| سال چاپ        | {year}        |
-| کشور           | {country}     |
-| ژانر           | {genre}       |
-| امتیاز         | {rating}      |
+| نام اثر | {title} |
+| نویسنده | {author} |
+| سال چاپ | {year} |
+| کشور | {country} |
+| ژانر | {genre} |
+| امتیاز | {rating} |
 """
     return table
 
