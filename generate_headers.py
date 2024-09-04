1d7ca96dca1b0fc2eac12003c78330eb10f97975
Ali Rostami <rostamiev@gmail.com>
2024-09-04 14:01:35 +0200

    trying to automatically add header

--- generate_headers.py
+++ generate_headers.py
@@ -21,10 +21,25 @@ with open('table_of_posts.csv') as csvfile:
     for row in spamreader:
         file_path = os.path.join(folder_path, row[5] + '-' + row[6])
         print(file_path)
+        file_contents = ''
+        changed = False
         with open(file_path, 'r', encoding='utf-8') as file:
             file_contents = file.read()
             if 'نام اثر' not in file_contents:
                 header_for_book = generate_book_table(row[0],row[1],row[2],row[3],'رمان اجتماعی','⭐⭐⭐⭐☆ 4/5')
+                #print(header_for_book)
+                text = """toc: true\n---"""
+                file_contents = file_contents.replace(text, text+'\n\n' + header_for_book)
+                changed = True
+            
+        if changed:
+           print(file_contents)
+           with open(file_path, 'w', encoding='utf-8') as file:
+               pass
+               file.write(file_contents)
+        break
+        
+        
 
 
 
