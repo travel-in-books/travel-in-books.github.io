c18e1439d0a066efe53253983905828a6e1585c9
Ali Rostami <rostamiev@gmail.com>
2024-09-13 09:33:53 +0200

    starting converting chapters format

---
+++ convert_faslha.py
@@ -0,0 +1,31 @@
+import os
+import re
+
+def contains_number_dot(text):
+    # Regular expression to match a number followed by a dot
+    pattern = r'\d+\.'  
+    # Search for the pattern in the text
+    match = re.search(pattern, text)
+    # Return True if match is found, else False
+    return bool(match)
+
+output = ''
+content = ''
+title = ''
+with open('faslha','r') as file:
+    for line in file:      
+        if contains_number_dot(line):
+            if title != '':
+                output = output + f'''
+        <details>
+        <summary>{title}</summary>
+    {content}
+        </details>'''
+            title = line.rstrip()
+            content = ''
+        else:
+            content = content + line
+    
+    with open('converted_faslha', 'w') as fw:
+        fw.write(output) 
+        
